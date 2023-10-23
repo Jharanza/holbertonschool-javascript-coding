10b576c406c71ae38813c247ebd39f39c39293ea
Jhony Arana <Jharanza1986@gmail.com>
2023-10-22 21:53:48 -0500

    fix: Inserting code to handle error

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-  console.log(`code: ${response.statusCode}`); 
+  if (error) {
+    console.error(error);
+  }
+  console.log(`code: ${response.statusCode}`);
 });
