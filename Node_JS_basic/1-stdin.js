72851a9c8de972fd1185d197011a943626744942
Jhony Arana <jharanza1986@gmail.com>
2023-11-10 10:00:29 -0500

    fix: change the ccreate interface module

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,6 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout 
-});
+let rl = readline.createInterface(process.stdin, process.stdout);
 
 console.log('Welcome to Holberton School, what is your name?');
 
