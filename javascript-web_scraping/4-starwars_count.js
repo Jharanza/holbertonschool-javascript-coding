36c77d8cfd3e71fa4bba98a6014f431ca8a3e830
jhony arana <jharanza1986@gmail.com>
2024-02-14 14:09:28 -0500

    fix: delete call to the console

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,24 +6,21 @@
  * @moives data from the body response
  * @artillesInMovie count number of films where appear a character
  */
-const request = require('request');
+const request = require('request')
 
 const uri = process.argv[2];
-console.log(uri);
 
 request(uri, (err, res, body) => {
   if (err) console.log(err);
-
   else if (res.statusCode !== 200) console.log(res.statusCode);
-
   else {
     const movies = JSON.parse(body).results;
-
     const antillesInMovie = movies.reduce((count, movie) => {
-      if (movie.characters.some(character => character.includes('/18/'))) count += 1;
+      if (movie.characters.some(character => character.includes('/18/'))) {
+        count += 1;
+      }
       return count;
-    }, 0);
-    
-    console.log(antillesInMovie);
+  }, 0);
+  console.log(antillesInMovie)
   }
-});
+})
