151e805e77b410167b25d592d0d808b09f91cbeb
jhony arana <jharanza1986@gmail.com>
2024-02-14 16:05:55 -0500

    feat: Change form to show the data

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 /**
- *
+ * Show the completed tasks
+ * @request module request
+ * @toDos data from the body transform in an object
+ * @completedTask object with the tasks completed
  */
 const request = require('request');
 
@@ -14,17 +17,12 @@ request(uri, (err, res, body) => {
   else {
     const toDos = JSON.parse(body);
 
-    const completedTask = {};
+    const completedTask = toDos.reduce((result, toDos) => {
+      if (toDos.completed) result[toDos.userId] = (result[toDos.userId] || 0) + 1;
 
-    toDos.forEach((toDo) => {
-      if (toDo.completed) {
-        const userId = toDo.userId;
-        if (completedTask[userId]) completedTask[userId]++;
-        else completedTask[userId] = 1;
-      }
-    });
+      return result;
+    }, {});
 
-    const output = JSON.stringify(completedTask, null, 1);
-    console.log(output);
+    console.log(completedTask);
   }
 });
