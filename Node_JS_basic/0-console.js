ac60c4204929bfbbb7b5deb53de93f8d5708b81b
jhony arana <jharanza1986@gmail.com>
2024-02-27 12:16:18 -0500

    feat: function that prints in the STDOUT a message

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * Prints in stdout an string argument
+ * @str argument that will be displayed
+ */
+
+module.exports = function displayMessage(str) {
+  process.stdout.write(str);
+  console.log();
+};
