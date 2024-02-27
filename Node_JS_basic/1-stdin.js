42662360778c019b5f66addcaba820448956a233
jhony arana <jharanza1986@gmail.com>
2024-02-27 15:29:21 -0500

    fix: Add semicolon

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,14 +3,14 @@
  * Execute an program through the command line
  * @param {name} data from the input
  */
-const os = require('os')
+const os = require('os');
 const readLine = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readLine.question('Welcome to Holberton School, what is your name?' + os.EOL, (name) => {
+readLine.question(`Welcome to Holberton School, what is your name?${os.EOL}`, (name) => {
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing' + os.EOL);
+  console.log('This important software is now closing');
   readLine.close();
 });
