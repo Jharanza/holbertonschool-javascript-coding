744fc4b9b2f6b7da5a991edc8bc46ab191375563
jhony arana <jharanza1986@gmail.com>
2024-02-27 15:27:22 -0500

    feat: Add library os to manage the end of line

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,14 +3,14 @@
  * Execute an program through the command line
  * @param {name} data from the input
  */
-
+const os = require('os')
 const readLine = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readLine.question('Welcome to Holberton School, what is your name?\n', (name) => {
+readLine.question('Welcome to Holberton School, what is your name?' + os.EOL, (name) => {
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
+  console.log('This important software is now closing' + os.EOL);
   readLine.close();
 });
