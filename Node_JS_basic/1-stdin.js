c00f74fbcf0873b5a2b352201a82bbc747c59bf7
jhony arana <jharanza1986@gmail.com>
2024-02-28 21:17:19 -0500

    Feat: add an event listener

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,14 +3,18 @@
  * Execute an program through the command line
  * @param {name} data from the input
  */
-const os = require('os');
-const readLine = require('readline').createInterface({
+const readline = require('readline');
+
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readLine.question(`Welcome to Holberton School, what is your name?${os.EOL}`, (name) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
   console.log('This important software is now closing');
-  readLine.close();
 });
