6c7c5c1e874247cd68e725df7695ad7f0d5486e3
jhony arana <jharanza1986@gmail.com>
2024-02-29 00:23:55 -0500

    feat: Managing the isTTY value

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 /**
- * Execute an program through the command line
- * @param {name} data from the input
+ * Execute a program through the command line or a pipeline
+ * @param {string} input data from the input
  */
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const { isTTY } = process.stdin;
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-});
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
+if (isTTY) {
+  rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
+    console.log(`Your name is: ${input}`);
+    rl.close();
+  });
+} else {
+  rl.write('Welcome to Holberton School, what is your name?\n');
+  let input = '';
+  process.stdin.on('data', (chunk) => {
+    input += chunk;
+  });
+  rl.write(`Your name is: ${input.trim()}`);
+}
+rl.on('close', () => console.log('This important software is now closing'));
